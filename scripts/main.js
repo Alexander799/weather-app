f1b4ee88468228112bb3c916f572496a9156eec5
Alexander799 <sashkakovalev63@mail.com>
2020-11-10 15:52:31 +0200

    Created filling the card with data from the server

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,16 @@ btn.onclick = () => {
     return cityName = document.getElementById('city-input');
 };
 console.log(cityName.value);*/
-let cityName = `Kyiv`;
+//https://openweathermap.org/img/wn/.png
+let cityName = `Kramatorsk`;
 fetch(`http://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1051f6ef13238541521ff75635f44f34`)
     .then(response => { return response.json() })
-    .then(data => { console.log(data) })
+    .then(data => {
+        document.getElementById('city').textContent = data.name;
+        document.getElementById('degrees').innerHTML += `: ${(data.main.temp - 273).toFixed(2)}&#176`;
+        document.getElementById('weather-icon').setAttribute(`src`, `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+        document.getElementById('description').textContent = data.weather[0].description;
+        document.getElementById('direction').innerHTML = `${data.wind.deg}&#176`;
+        document.getElementById('speed').textContent = `${data.wind.speed} m/s`;
+    })
     .catch(err => { console.log(err) })
