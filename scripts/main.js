6f9640bba39755aafe225365ecb455cdb2d5d407
Alexander799 <sashkakovalev63@mail.com>
2020-11-24 23:15:12 +0200

    added function direction

--- scripts/main.js
+++ scripts/main.js
@@ -23,12 +23,32 @@ function operation(bool) {
                     document.getElementById('degrees').innerHTML = `Temperature, C: ${(data.main.temp - 273).toFixed(2)}&#176`;
                     document.getElementById('weather-icon').setAttribute(`src`, `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
                     document.getElementById('description').textContent = data.weather[0].description;
-                    document.getElementById('direction').innerHTML = `${data.wind.deg}&#176`;
+                    document.getElementById('direction').innerHTML = direction(data.wind.deg);
                     document.getElementById('speed').textContent = `${data.wind.speed} m/s`;
                 })
                 .catch(err => { console.log(err) })
             document.getElementById('user-input').value = "";
             document.getElementsByClassName('weather-block')[0].style.display = 'flex';
         }
+    }
+}
+
+function direction(deg) {
+    if (deg > 337 && deg <= 360 || deg >= 0 && deg <= 22) {
+        return 'North';
+    } else if (deg >= 22 && deg <= 67) {
+        return 'North-East';
+    } else if (deg > 67 && deg <= 112) {
+        return 'East';
+    } else if (deg > 112 && deg <= 157) {
+        return 'East-South';
+    } else if (deg > 157 && deg <= 202) {
+        return 'South';
+    } else if (deg > 202 && deg <= 247) {
+        return 'South-West';
+    } else if (deg > 247 && deg <= 292) {
+        return 'West';
+    } else if (deg > 292 && deg <= 337) {
+        return 'North-West';
     }
 }
