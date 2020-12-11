b3a8608ceb3bf1e629a3c6c738b8ffb5a7761810
Alexander799 <sashkakovalev63@mail.com>
2020-12-11 11:44:10 +0200

    added check user input

--- scripts/main.js
+++ scripts/main.js
@@ -10,25 +10,29 @@ document.addEventListener('keyup', function(event) {
 
 function operation(bool) {
     if (bool) {
-        document.getElementById('city').textContent = document.getElementById('user-input');
-        if (document.getElementById('city').innerHTML == 'city') {
-            document.getElementsByClassName('weather-block')[0].style.display = 'none';
+        const USERINPUT = document.getElementById('user-input');
+        if (USERINPUT.value.trim().length > 0) {
+            document.getElementById('city').textContent = USERINPUT.value;
+            if (document.getElementById('city').innerHTML == 'city') {
+                document.getElementsByClassName('weather-block')[0].style.display = 'none';
+            } else {
+                cityName = USERINPUT.value.trim();
+                fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1051f6ef13238541521ff75635f44f34`)
+                    .then(response => { return response.json() })
+                    .then(data => {
+                        document.getElementById('city').textContent = data.name;
+                        document.getElementById('degrees').innerHTML = `Temperature, C: ${(data.main.temp - 273).toFixed(2)}&#176`;
+                        document.getElementById('weather-icon').setAttribute(`src`, `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+                        document.getElementById('description').textContent = data.weather[0].description;
+                        document.getElementById('direction').innerHTML = direction(data.wind.deg);
+                        document.getElementById('speed').textContent = `${data.wind.speed} m/s`;
+                    })
+                    .catch(err => { console.log(err) })
+                USERINPUT.value = "";
+                document.getElementsByClassName('weather-block')[0].style.display = 'flex';
+            }
         } else {
-            cityName = document.getElementById('user-input');
-
-            fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName.value}&appid=1051f6ef13238541521ff75635f44f34`)
-                .then(response => { return response.json() })
-                .then(data => {
-                    document.getElementById('city').textContent = data.name;
-                    document.getElementById('degrees').innerHTML = `Temperature, C: ${(data.main.temp - 273).toFixed(2)}&#176`;
-                    document.getElementById('weather-icon').setAttribute(`src`, `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
-                    document.getElementById('description').textContent = data.weather[0].description;
-                    document.getElementById('direction').innerHTML = direction(data.wind.deg);
-                    document.getElementById('speed').textContent = `${data.wind.speed} m/s`;
-                })
-                .catch(err => { console.log(err) })
-            document.getElementById('user-input').value = "";
-            document.getElementsByClassName('weather-block')[0].style.display = 'flex';
+            USERINPUT.value = "";
         }
     }
 }
